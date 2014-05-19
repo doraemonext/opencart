9c339797029cec90f8ce6e1f06433bf8c513d0e6
doraemonext <doraemonext@gmail.com>
2014-05-19 09:40:49 +0800

    增加主题文件

---
+++ upload/admin/language/english/module/tm_revolutoin_slideshow.php
@@ -0,0 +1,84 @@
+<?php
+// Heading
+$_['heading_title']       = 'TM Revolution Slideshow';
+$_['heading_banner_title'] = 'TM Revolution Slideshow - Banners';
+// Text
+$_['button_save_stay']	  = 'Save & Stay';
+$_['text_module']         = 'Modules';
+$_['text_success']        = 'Success: You have modified module tm_revolutoin_slideshow!';
+$_['text_content_top']    = 'Content Top';
+$_['text_content_bottom'] = 'Content Bottom';
+$_['text_column_left']    = 'Column Left';
+$_['text_column_right']   = 'Column Right';
+$_['text_mainmenu']   = 'Main Menu';
+$_['text_slideshow']   = 'Slideshow';
+$_['text_footer_top']   = 'Footer Top';
+$_['text_footer_center']   = 'Footer Center';
+$_['text_footer_bottom']   = 'Footer Bottom';
+$_['text_default'] = 'Default';
+$_['text_all_layout'] = 'All layout';
+$_['text_header_top'] = 'Header Top';
+$_['text_header_bottom'] = 'Header Bottom';
+$_['text_tm_revolutoin_slideshow_module'] = 'Slideshow Module';
+$_['text_tm_revolutoin_slideshow_banner'] = 'Manage Banner';
+$_['text_tm_revolutoin_slideshow_support'] = 'Support';
+$_['text_banner_image'] = 'Banner Images';
+$_['text_image_manager'] = 'Image Manager';
+$_['text_browse'] = 'Browse';
+$_['text_clear'] = 'Clear';
+$_['text_input_product_name'] = 'Input product name';
+$_['text_select_banner'] = 'Select a banner';
+$_['text_default'] = 'Default';
+$_['text_white_space'] = 'Scale with white space';
+$_['text_resize_width'] = 'Fill according to width';
+$_['text_resize_height'] = 'Fill according to height';
+
+
+$_['all_page']   = 'All Page';
+// Entry
+
+
+
+$_['entry_show_slider_link'] = 'Show slider link:<br><span class="help">Show/hide the URL where you go by clicking your slide.</span>';
+
+$_['entry_strip_html_code'] = 'Strip HTML Code:<br/><span class="help">Enable/Disable strip HTML code on description</span>';
+
+$_['entry_slider_caption'] = 'Slideshow Caption';
+$_['entry_store'] = 'Stores: ';
+$_['entry_name'] = 'Name: ';
+$_['entry_layout']        = 'Layout:';
+$_['entry_position']      = 'Position:';
+$_['entry_status']        = 'Status:';
+$_['entry_sort_order']    = 'Sort Order:';
+$_['entry_banner']		= '<font style="color:#E25203">Banner: <br><span class="help">Select a banner to get images for slideshow</span></font>';
+$_['entry_module_width'] = 'Module Width: <br/><span class="help">You need to input number with type "px,%, auto", for example: 980px, 100% or auto</span>';
+$_['entry_module_height'] = 'Module Height:  <br/><span class="help">You need to input number with type "px,%, auto", for example: 780px, 60% or auto</span>';
+$_['entry_main_width'] = 'Main Image Width: <br><span class="help">Set Width of Main Image Slideshow (input number value), for example: 980</span>';
+$_['entry_main_height'] = 'Main Image Height:<br><span class="help">Set Height of Main Image Slideshow (input number value), for example: 380</span>';
+
+
+
+
+
+
+$_['button_add_new_block'] = 'Add new slideshow';
+$_['button_save_stay'] = 'Save & Stay';
+$_['tab_block'] = 'Slideshow ';
+$_['tab_banner'] = 'Banner ';
+
+
+$_['entry_title'] = 'Title:<br/><span class="help">Limit 100 charecters</span> ';
+$_['entry_description'] = 'Description: ';
+$_['entry_link'] = 'Link: ';
+$_['entry_image'] = 'Image: ';
+$_['entry_custom_position'] = 'Custom Position: <br><span class="help">Input the custom position of your theme, for example: slideshow, content_left,...<br/>Set empty "Custom Position" when you want use Opencart Default positions.</span>';
+$_['entry_limit'] = 'Limit Slider Item: ';
+$_['entry_title_max_chars'] = 'Title Max Chars: ';
+$_['entry_description_max_chars'] = 'Description Max Chars: ';
+$_['column_name'] = 'Name';
+$_['column_status'] = 'Status';
+$_['column_action'] = 'Action';
+
+// Error
+$_['error_permission']    = 'Warning: You do not have permission to modify module tm_revolutoin_slideshow!';
+?>
